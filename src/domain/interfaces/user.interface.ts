5725550b4f170575070c357c805375df04b6ed8a
victorsz08 <joaovictorsal777@gmail.com>
2025-09-03 16:59:31 -0300

    feat(user): add user interface with methods repo

---
+++ src/domain/interfaces/user.interface.ts
@@ -0,0 +1,31 @@
+import { UserEntity } from "../entities/user.entity";
+
+export type TUserListOuput = {
+    users: UserEntity[];
+    total: number;
+    offset: number;
+    pages: number;
+};
+
+export type TUserListInput = {
+    page: number;
+    offset: number;
+    keywords?: string;
+};
+
+export interface UserInterface {
+    create(user: UserEntity): Promise<void>;
+    find(id: string): Promise<UserEntity | undefined>;
+    list(query: TUserListInput): Promise<TUserListOuput>;
+    findByUsername(username: string): Promise<UserEntity | undefined>;
+    update(
+        input: Pick<
+            UserEntity,
+            "id" | "username" | "firstName" | "lastName" | "updatedAt"
+        >,
+    ): Promise<void>;
+    updatePassword(
+        input: Pick<UserEntity, "id" | "password" | "updatedAt">,
+    ): Promise<void>;
+    delete(id: string): Promise<void>;
+}
